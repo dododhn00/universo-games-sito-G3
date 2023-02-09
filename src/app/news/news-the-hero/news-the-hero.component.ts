77c95411e37f793b8f49af1889b48a501a487f7f
RemonIssa <123636134+RemonIssa@users.noreply.github.com>
2023-02-09 15:15:32 +0100

    The hero inizio

--- src/app/news/news-the-hero/news-the-hero.component.ts
+++ src/app/news/news-the-hero/news-the-hero.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { SingleNews } from 'src/app/model/singleNews';
+import { NewsService } from '../service/news.service';
 
 @Component({
   selector: 'app-news-the-hero',
   templateUrl: './news-the-hero.component.html',
   styleUrls: ['./news-the-hero.component.css']
 })
-export class NewsTheHeroComponent {
+export class NewsTheHeroComponent implements OnInit{
 
+  heroNews$!: Observable<SingleNews[]>
+
+  constructor(private newsService: NewsService) {}
+
+  ngOnInit(): void {
+    this.heroNews$ = this.newsService.getNews();
+  }
 }
