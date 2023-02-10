5d351de40a8744823bbbb9e199d5de019cce631c
RemonIssa <123636134+RemonIssa@users.noreply.github.com>
2023-02-10 09:18:04 +0100

    aggiunto search

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { CatalogService } from 'src/app/catalog/service/catalog.service';
+import { SingleNews } from 'src/app/model/singleNews';
+import { NewsService } from 'src/app/news/service/news.service';
+
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit{
 
+  data!: SingleNews[]
+
+  searchText = '';
+
+  form: FormGroup = new FormGroup({
+    keyword: new FormControl(null, [
+    Validators.required
+    ])
+  }); 
+
+  constructor(private newsService: NewsService, catalogService:CatalogService) {}
+
+  ngOnInit(): void {
+    this.newsService.getNews().subscribe(data => this.data = data);
+  }
 }
