b01539deb302c14f5fb0b6dfd80f14231e8654de
PietroS <stivanellop@yahoo.com>
2023-02-13 15:47:38 +0100

    footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
-  theme: {
-    extend: {},
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+    theme: {
+    extend: {
+      colors: {
+        'regal-blue': '#243c5a',
+        'footer': '#7B7A7A',
+      },
+    },
   },
   plugins: [
     require('@tailwindcss/line-clamp'),
