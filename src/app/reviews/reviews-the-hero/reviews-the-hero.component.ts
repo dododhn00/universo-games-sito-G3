8c78b1d183917d0da4b8d9887e74e6ddd0968836
dododhn00 <ramanuccio@gmail.com>
2023-02-13 12:14:51 +0100

    Pagina recensioni

--- src/app/reviews/reviews-the-hero/reviews-the-hero.component.ts
+++ src/app/reviews/reviews-the-hero/reviews-the-hero.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Review } from 'src/app/model/review';
+import { ReviewsService } from '../service/reviews.service';
 
 @Component({
   selector: 'app-reviews-the-hero',
@@ -6,4 +8,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./reviews-the-hero.component.css']
 })
 export class ReviewsTheHeroComponent {
+
+  reviews!: Review[];
+
+  constructor(
+    private service: ReviewsService
+  ) {}
+
+  ngOnInit(): void {
+
+    this.service.getReviews().subscribe(data => {
+      this.reviews = data.sort((a, b) => {
+        const dt1 = Date.parse(a.publicationDate);
+        const dt2 = Date.parse(b.publicationDate);
+        return dt2 - dt1
+      })
+    });
+
+  }
+
 }
