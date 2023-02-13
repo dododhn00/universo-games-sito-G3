030cafa4bea65803a08263c407daf8788b2e9365
PietroS <stivanellop@yahoo.com>
2023-02-13 12:11:31 +0100

    newsdetail page implemented + responsive news list

--- src/app/news/news-detail-content/news-detail-content.component.ts
+++ src/app/news/news-detail-content/news-detail-content.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+import { SingleNews } from 'src/app/model/singleNews';
+import { NewsService } from '../service/news.service';
 
 @Component({
   selector: 'app-news-detail-content',
   templateUrl: './news-detail-content.component.html',
   styleUrls: ['./news-detail-content.component.css']
 })
-export class NewsDetailContentComponent {
+export class NewsDetailContentComponent implements OnInit{
 
+  constructor(private newsService: NewsService, private router: Router, private route: ActivatedRoute){}
+  
+  news$!: Observable<SingleNews>;
+  
+  myDate = "26-02-2012";
+  myaDate = this.myDate.split("-");
+  newDate = new Date( +this.myaDate[2], +this.myaDate[1] -1, +this.myaDate[0]);
+  
+    ngOnInit(): void {
+      this.route.params.subscribe((p)=> {
+        const id = p['id'];
+        this.news$ = this.newsService.getSingleNews(id);
+      });
+
+      console.log('dataa:', this.newDate.toLocaleDateString());
+    }
 }
+
--- src/app/news/news-detail-hero/news-detail-hero.component.ts
+++ src/app/news/news-detail-hero/news-detail-hero.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+import { SingleNews } from 'src/app/model/singleNews';
+import { NewsService } from '../service/news.service';
 
 @Component({
   selector: 'app-news-detail-hero',
   templateUrl: './news-detail-hero.component.html',
   styleUrls: ['./news-detail-hero.component.css']
 })
-export class NewsDetailHeroComponent {
 
+export class NewsDetailHeroComponent implements OnInit{
+
+  constructor(private newsService: NewsService, private router: Router, private route: ActivatedRoute){}
+  
+  news$!: Observable<SingleNews>;
+  
+    ngOnInit(): void {
+      this.route.params.subscribe((p)=> {
+        const id = p['id'];
+        this.news$ = this.newsService.getSingleNews(id);
+      });
+      
+    }
 }
